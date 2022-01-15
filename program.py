0ffcf3a1d4a391365d97b9b52ca26a2d9849840d
ThirdBlood <65395090+ThirdBlood@users.noreply.github.com>
2022-01-15 11:57:46 +0800

    Fixed Bugs
    修复幻塔出现位置反常等问题

--- program.py
+++ program.py
@@ -1,37 +1,50 @@
 # coding = utf-8
 
-def str_insert(str_origin, pos, str_add):
-    str_list = list(str_origin)  # 字符串转list
-    str_list.insert(pos, str_add)  # 在指定位置插入字符串
-    str_out = ''.join(str_list)  # 空字符连接
-    return str_out
+
+def cut(string, char):
+    """
+    Cut函数
+    以 char 分割 string 并添加“幻塔”
+    char: 分割字符
+    string: 待处理的字符串
+    """
+    index = 0
+    string_list = string.split(char)
+    while 1:
+        if string_list[0] == string_list[-1]:
+            if '幻塔' not in string_list[0]:
+                string_list[index] = string_list[index] + '幻塔'
+            break
+        if string_list[index] != string_list[-1]:
+            if '原神' not in string_list[index] \
+                    and '幻塔' not in string_list[index]:
+                string_list[index] = string_list[index] + '幻塔'
+        else:
+            break
+        index += 1
+    string = char.join(string_list)
+    return string
 
 
 def exchange(string):
     """
     Exchange函数
     将原神的评论转变为幻塔的评论
-    string: 待处理的评论
+    string: 待处理的字符串
     """
     import re  # 我需要re模块来进行操作qwq
-    string = re.sub('原神', '幻塔', string)
-    if '，' not in string:  # 如果没有断句，则直接处理输出
-        string = string + '幻塔，。'
-        return string
+
     # 开始在断句的前面添加“幻塔”
-    index = 0
-    while 1:
-        index += 1
-        if string[index] == '，':
-            string = str_insert(string, index, '幻塔')
-            index += 2  # 防止死循环（刚才我已经TMD因为这个物理内存耗尽了，TNND，变量里全是“幻塔”两个字）
-        elif string[index] == string[-1]:
-            break
-    string = string + '幻塔，。'
-    return string
+    string = string.rstrip('，。？！')  # 去除结尾的标点符号
+    string = cut(string, '，')  # 处理逗号
+    string = cut(string, '。')  # 处理句号
+    string = cut(string, '？')  # 处理问号
+    string = cut(string, '！')  # 处理叹号
+    string = re.sub('原神', '幻塔', string)  # 替换“原神”为“幻塔”
+    return string + '，。'
 
 
 if __name__ == '__main__':
     keyboard = input('请输入：')
-    print(exchange(keyboard))
+    print('\n输出：' + exchange(keyboard))
     input('按回车键继续...')
